673f92e934184a0c96a03c0adfc4a769bfea8b0e
Evan Mitchell <43766252+evanmitchell777@users.noreply.github.com>
2023-01-28 23:40:21 -0600

    Update bash_script_deployer.py

--- bash_script_deployer.py
+++ bash_script_deployer.py
@@ -1,6 +1,7 @@
+#this prorgam will help me speed up the use of my Bash scripts for my IS577 class
 import subprocess
-
-option=int(input("Select option 1-3: 1. search for usernames 2. list all invalid IP, 3. list all invalid username: "))
+print("Select option 1-3:")
+option=int(input(" 1. search for usernames 2. list all invalid IP, 3. list all invalid username: "))
 
 if option==1:
     subprocess.call('./username_ip_search')
